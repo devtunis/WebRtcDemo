bf5cd835f86916671612a943f7a2ebec85165998
Kc12233 <xyy45557@gmail.com>
2026-02-03 02:17:06 +0100

    Update socket.js

--- demo/src/SocketProvider/socket.js
+++ demo/src/SocketProvider/socket.js
@@ -1,11 +1,12 @@
 import io from "socket.io-client"
 
 
-const socket = io("http://localhost:8080/",
+const socket = io("https://webrtcdemo-chop.onrender.com",
     {
         autoConnect:false,
          auth: { key: "super-secret-123" }
     }
 )
+
 
 export default  socket
