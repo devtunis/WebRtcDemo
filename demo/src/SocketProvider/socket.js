47f78fc99117245ac25e8f3a165d3b175f516596
Ghaith <121437369+devtunis@users.noreply.github.com>
2026-02-03 02:55:14 +0100

    Update socket.js

--- demo/src/SocketProvider/socket.js
+++ demo/src/SocketProvider/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client"
 
 
-const socket = io("http://localhost:8080",
+const socket = io("https://webrtcdemo-xo84.onrender.com",
     {
         autoConnect:false,
          auth: { key: "super-secret-123" }
@@ -10,4 +10,5 @@ const socket = io("http://localhost:8080",
 
 
 export default  socket
+
 
